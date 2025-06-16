e8e84b3a2e29d83fdd0184b3b46a313c8484dada
Omar Flores <omar533330@gmail.com>
2025-06-15 22:15:59 -0600

    Playing around

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+	site: "https://example.com",
+	image: {
+		domains: ["avatars.githubusercontent.com"]
+	},
+	integrations: [mdx(), sitemap()],
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
