6a8a927b8a43dbf1d66ad904dba00fa8d0e000a1
Omar Flores <omar533330@gmail.com>
2025-06-16 23:20:44 -0600

    Layout and themes V1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "yoyojambo's blog";
+export const SITE_TITLE = "my blog";
 export const SITE_DESCRIPTION = "My page, still in progress!";
