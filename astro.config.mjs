9342d2ba509513d5cfd6c4e3d00fae0b63548e95
Omar Flores <omar533330@gmail.com>
2026-05-12 00:14:40 -0600

    Fix astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://yoyojambo.com",
@@ -10,6 +12,7 @@ export default defineConfig({
         domains: ["avatars.githubusercontent.com"]
     },
 	output: "static",
+	adapter: cloudflare(),
     integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
